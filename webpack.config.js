e1675040d1da06ccb0dbb4117d3f2dacb2efb4ce
Kerminate <kepeilin@outlook.com>
2018-10-14 13:19:56 +0800

    feat: webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports = {
-  mode: 'development',
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  target: 'web',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'bundle.js',
@@ -13,10 +17,60 @@ module.exports = {
       {
         test: /.vue$/,
         loader: 'vue-loader'
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader']
+      },
+      {
+        test: /\.(gif|jpg|jpeg|svg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              // fallback 默认值就是 file-loader，当文件大小大于 1024b时，使用 file-loader 
+              fallback: 'file-loader',
+              name: '[name]-aaa.[ext]'
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLPlugin()
   ]
 }
+
+if (isDev) {
+  config.mode = 'development'
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    port: 8000,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true
+    },
+    open: true, // webpack 启动后自动打开网站
+    hot: true,
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+} else {
+  config.mode = 'production'
+}
+
+module.exports = config
