d77645ed58db85b471632a5c643eb1ccf86b88f6
Kerminate <kepeilin@outlook.com>
2018-10-17 22:55:53 +0800

    feat: add extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -9,7 +10,7 @@ const config = {
   target: 'web',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8].js',
     path: path.join(__dirname, 'dist')
   },
   module: {
@@ -23,31 +24,13 @@ const config = {
         loader: 'babel-loader'
       },
       {
-        test: /.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /.(styl|stylus)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          'stylus-loader'
-        ]
-      },
-      {
         test: /\.(gif|jpg|jpeg|svg|png)$/,
         use: [
           {
             loader: 'url-loader',
             options: {
               limit: 1024,
-              // fallback 默认值就是 file-loader，当文件大小大于 1024b时，使用 file-loader 
+              // fallback 默认值就是 file-loader，当文件大小大于 1024b时，使用 file-loader
               fallback: 'file-loader',
               name: '[name]-aaa.[ext]'
             }
@@ -70,6 +53,7 @@ const config = {
 if (isDev) {
   config.mode = 'development'
   config.devtool = '#cheap-module-eval-source-map'
+  config.output.filename = '[name[.[chunkhash:8].js'
   config.devServer = {
     port: 8000,
     host: '0.0.0.0',
@@ -77,14 +61,47 @@ if (isDev) {
       errors: true
     },
     open: true, // webpack 启动后自动打开网站
-    hot: true,
+    hot: true
   }
+  config.module.rules.push({
+    test: /.(styl|stylus)$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+      'stylus-loader'
+    ]
+  })
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   )
 } else {
   config.mode = 'production'
+  config.module.rules.push({
+    test: /.(styl|stylus)$/,
+    use: ExtractPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true
+          }
+        },
+        'stylus-loader'
+      ]
+    })
+  })
+  config.plugins.push(
+    new ExtractPlugin('styles.[hash:8].css')
+  )
 }
 
 module.exports = config
